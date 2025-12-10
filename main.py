fc4d8f0a9f6fcf63884a43569e7e3be80fb96c81
Nikita <ntarasov0326@icloud.com>
2025-12-10 18:48:40 +0300

    Refactoring: added functions main and play_round

--- main.py
+++ main.py
@@ -3,14 +3,7 @@
 def separator():
     print("-" * 30)
 
-print("Welcome to my computer quiz!")
-
-playing = input("Do you want to play? ").lower().strip()
-if playing != "yes":
-    quit(1)
-print("Okay! Let`s play :)")
-
-questions = [
+QUESTIONS = [
     ("What does CPU stand for? ", "central processing unit"),
     ("What does GPU stand for? ", "graphics processing unit"),
     ("What does RAM stand for? ", "random access memory"),
@@ -20,22 +13,42 @@ questions = [
     ("What does API stand for? ", "application programming interface"),
 ]
 
-while playing == "yes":
+def play_round() -> None:
     score = 0
-    for question, answer in questions:
+    for question, answer in QUESTIONS:
         separator()
         response = input(question).lower().strip()
         if response == answer:
             print("Correct!")
             score += 1
         else:
-            print("Incorrect!")
+            print(f"Incorrect! The correct answer is: {answer}.")
+
     separator()
     print(f"You got {score} questions correct!")
-    print(f"You got {(score / len(questions)) * 100:.1f}%.")
+    print(f"You got {(score / len(QUESTIONS)) * 100:.1f}%.")
     separator()
 
-    playing = input("Do you want to play again? ").lower().strip()
-separator()
-print("\n")
-print("Thanks for the game!")
+    return
+
+def main() -> None:
+    print("Welcome to my computer quiz!")
+
+    playing = input("Do you want to play? ").lower().strip()
+    if playing not in ("yes", "y"):
+        print("Okay, maybe next time!")
+        return
+    print("Okay! Let`s play :)")
+
+    while True:
+        play_round()
+
+        playing = input("Do you want to play again? ").lower().strip()
+        if playing not in ("yes", "y"):
+            break
+
+    print("\n")
+    print("Thanks for the game!")
+
+if __name__ == "__main__":
+    main()
